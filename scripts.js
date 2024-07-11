48d5ce9747b7476ea699ea99f833702f950c0284
Pasha <pavik.varganov@gmail.com>
2024-07-11 22:42:02 +0300

    version 1.1 change switch theme button add filters

--- scripts.js
+++ scripts.js
@@ -23,8 +23,8 @@ function initializeTeams() {
                 <div class="team">
                     <img class="team-icon" src="ball.png" alt="Placeholder Icon">
                     <div class="team-info">
-                        <h2>Натисніть кнопку щоб обрати команди</h2>
-                        <p>Ліга: N/A</p>
+                        <h2 class="${document.body.classList.contains('dark-mode') ? 'dark-mode' : ''}">Натисніть кнопку щоб обрати команди</h2>
+                        <p class="${document.body.classList.contains('dark-mode') ? 'dark-mode' : ''}">Ліга</p>
                     </div>
                     <div class="rating">
                         <img src="0stars.png" alt="Rating: 0">
@@ -36,7 +36,24 @@ function initializeTeams() {
 }
 
 function shuffleTeams() {
-    const shuffledTeams = teamsData.sort(() => 0.5 - Math.random()).slice(0, 4);
+    const ratingFilter = document.getElementById('rating-filter').value;
+    const selectedLeagues = Array.from(document.querySelectorAll('input[name="league"]:checked')).map(checkbox => checkbox.value);
+    // const mode = document.querySelector('input[name="mode"]:checked').value;
+
+    let filteredTeams = teamsData.filter(team => parseFloat(team.rating) >= ratingFilter);
+    if (selectedLeagues.length > 0) {
+        filteredTeams = filteredTeams.filter(team => selectedLeagues.includes(team.league));
+    }
+
+    let shuffledTeams = filteredTeams.sort(() => 0.5 - Math.random()).slice(0, 4);
+    // if (mode === 'real') {
+    //     const highRatedTeams = filteredTeams.filter(team => parseFloat(team.rating) >= 4.5).slice(0, 2);
+    //     const lowRatedTeams = filteredTeams.filter(team => parseFloat(team.rating) < 4.5).slice(0, 2);
+    //     shuffledTeams = [...highRatedTeams, ...lowRatedTeams];
+    // } else {
+    //     shuffledTeams = filteredTeams.sort(() => 0.5 - Math.random()).slice(0, 4);
+    // }
+
     const teamsContainer = document.getElementById('teams');
     teamsContainer.innerHTML = '';
     shuffledTeams.forEach((team, index) => {
@@ -45,8 +62,8 @@ function shuffleTeams() {
             <div class="team">
                 <img class="team-icon" src="${team.icon}" alt="${team.name} Icon">
                 <div class="team-info">
-                    <h2>${playerName} грає ${team.name}</h2>
-                    <p>Ліга: ${team.league}</p>
+                    <h2 class="${document.body.classList.contains('dark-mode') ? 'dark-mode' : ''}">${playerName} грає ${team.name}</h2>
+                    <p class="${document.body.classList.contains('dark-mode') ? 'dark-mode' : ''}">${team.league}</p>
                 </div>
                 <div class="rating">
                     <img src="${team.ratingico}" alt="Rating: ${team.rating}">
@@ -78,8 +95,8 @@ function loadTeams() {
                 <div class="team">
                     <img class="team-icon" src="${team.icon}" alt="${team.name} Icon">
                     <div class="team-info">
-                        <h2>${playerName} грає ${team.name}</h2>
-                        <p>Ліга: ${team.league}</p>
+                        <h2 class="${document.body.classList.contains('dark-mode') ? 'dark-mode' : ''}">${playerName} грає ${team.name}</h2>
+                        <p class="${document.body.classList.contains('dark-mode') ? 'dark-mode' : ''}">${team.league}</p>
                     </div>
                     <div class="rating">
                         <img src="${team.ratingico}" alt="Rating: ${team.rating}">
@@ -92,9 +109,20 @@ function loadTeams() {
 
 function toggleTheme() {
     document.body.classList.toggle('dark-mode');
-    document.querySelectorAll('.container, button, .team-placeholder').forEach(element => {
+    document.querySelectorAll('.container, button, .team-placeholder, .team-info h2, .team-info p, label, h1').forEach(element => {
         element.classList.toggle('dark-mode');
     });
+
+    const themeIcon = document.getElementById('theme-toggle-icon');
+    if (document.body.classList.contains('dark-mode')) {
+        themeIcon.src = 'light_mode_icon.png';
+    } else {
+        themeIcon.src = 'dark_mode_icon.png';
+    }
+}
+
+function updateRatingValue() {
+    document.getElementById('rating-value').innerText = document.getElementById('rating-filter').value;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
